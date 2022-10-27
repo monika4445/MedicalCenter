34cc0521ebbaec58dccd5d75d7ee8ca05b655722
Monika <monikazaqaryann@gmail.com>
2022-10-27 14:27:17 +0400

    small chag

--- nodemailer.js
+++ nodemailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 let mailTransporter = nodemailer.createTransport({
 service: "gmail",
 auth: {
-   user: "monika.zaqaryan2002@gmail.com",
+   user: "monika.zaqaryan2002@gmail.com",//?
    pass: "qsjfffxzyzwunmgy"
 },
 tls: {
@@ -13,7 +13,7 @@ tls: {
 });
  
 let details = {
-   from: "monika.zaqaryan2002@gmail.com",
+   from: "monika.zaqaryan2002@gmail.com",// ?
    to: "monikazaqaryan55@gmail.com", //req.body.email
    subject: "You booked appointment successfully!",
    text: "Congrats! You booked appointment successfully! We will wait for you at your check-in time.",
